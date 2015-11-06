c4532c0691148a79f35289c1fbee861aa307e58e
Kristian Lauttamus <kristian.lauttamus@gmail.com>
2015-11-06 23:59:23 +0200

    First test

---
+++ DiabetesAssistant/src/test/java/DMCalculatorTest/DMCalculatorTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DMCalculatorTest;
+
+import com.lauttadev.diabetesassistant.DMCalculator;
+import java.util.ArrayList;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Kristian
+ */
+public class DMCalculatorTest {
+    private DMCalculator calc = new DMCalculator();
+    
+    public DMCalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testAverageBloodsugar() {
+        ArrayList<Integer> bloodsugars = new ArrayList<Integer>();
+        bloodsugars.add(50);
+        bloodsugars.add(1);
+        bloodsugars.add(25);
+        
+        assertEquals(25.3333f, calc.averageBloodsugar(bloodsugars), 0.0001);
+    }
+}
